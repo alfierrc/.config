f4763af05b66ec38ece3d302391056a9cfa8b821
Alfie <arrcoleman95@gmail.com>
2025-09-17 15:55:34 +0100

    nvim: refactor: seperate vim options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,2 @@
--- 2 space tab
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- leader = space
-vim.g.mapLeader = " "
-
--- load lazy.nvim
+require("vim-options")
 require("config.lazy")
-
--- persistant undo
-local undodir = vim.fn.stdpath('data') .. '/undodir'
-if vim.fn.isdirectory(undodir) == 0 then
-  vim.fn.mkdir(undodir, 'p')
-end
-vim.opt.undodir = undodir
-vim.opt.undofile = true
-
-
-
